611c92ecac08a2d1ff2667897cd80b9b9bf00fc8
unknown <nasyithmusyafa@gmail.com>
2023-05-18 14:28:54 +0700

    Cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_SMALL_URL: 'https://restaurant-api.dicoding.dev/images/small/',
-  BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/'
+  BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  CACHE_NAME: 'NgeResto-V1'
 }
 
 export default CONFIG
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,13 +1,31 @@
+import 'regenerator-runtime'
+import CacheHelper from './utils/cache-helper'
+
+const assetsToCache = [
+  './',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-144x144.png',
+  './icons/icon-192x192.png',
+  './icons/icon-256x256.png',
+  './icons/icon-512x512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon.ico',
+  './images/hero-image.jpg',
+  './index.html',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js'
+]
+
 self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...')
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]))
 })
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...')
+  event.waitUntil(CacheHelper.deleteOldCache())
 })
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request)
-
-  event.respondWith(fetch(event.request))
+  event.respondWith(CacheHelper.revalidateCache(event.request))
 })
