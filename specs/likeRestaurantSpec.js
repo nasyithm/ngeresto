1ad420c0d3d8546ce80d9b122ccbb409d9903c57
unknown <nasyithmusyafa@gmail.com>
2023-05-28 15:57:51 +0700

    First Part Karma Test

---
+++ specs/likeRestaurantSpec.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-undef */
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb'
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator'
+
+describe('Liking A Restaurant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>'
+  }
+
+  beforeEach(() => {
+    addLikeButtonContainer()
+  })
+
+  it('should show the like button when the restaurant has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 's1knt6za9kkfw1e867'
+      }
+    })
+
+    expect(
+      document.querySelector('[aria-label="like this restaurant"]')
+    ).toBeTruthy()
+  })
+
+  it('should not show the unlike button when the movie has not been liked before', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 's1knt6za9kkfw1e867'
+      }
+    })
+    expect(
+      document.querySelector('[aria-label="unlike this movie"]')
+    ).toBeFalsy()
+  })
+
+  it('should be able to like the movie', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 's1knt6za9kkfw1e867'
+      }
+    })
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'))
+
+    const restaurant = await FavoriteRestaurantIdb.getRestaurant(
+      's1knt6za9kkfw1e867'
+    )
+    expect(restaurant).toEqual({ id: 's1knt6za9kkfw1e867' })
+
+    FavoriteRestaurantIdb.deleteRestaurant('s1knt6za9kkfw1e867')
+  })
+
+  it('should not add a movie again when its already liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 's1knt6za9kkfw1e867'
+      }
+    })
+
+    // Tambahkan film dengan ID 1 ke daftar film yang disukai
+    await FavoriteRestaurantIdb.putRestaurant({ id: 's1knt6za9kkfw1e867' })
+
+    // Simulasikan pengguna menekan tombol suka film
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'))
+
+    // tidak ada film yang ganda
+    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([
+      { id: 's1knt6za9kkfw1e867' }
+    ])
+
+    FavoriteRestaurantIdb.deleteRestaurant('s1knt6za9kkfw1e867')
+  })
+})
